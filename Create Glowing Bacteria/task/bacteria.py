ee905cccf35a89abc6c74938c50d61a5c4085289
fvn70 <fvn_reg@mail.ru>
2022-08-07 17:09:40 +0700

    Stage 2/6: Cut the plasmid

--- Create Glowing Bacteria/task/bacteria.py
+++ Create Glowing Bacteria/task/bacteria.py
@@ -1,6 +1,15 @@
 dic = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
 
-first = input()
-second = [dic[x] for x in first]
-print(''.join(second))
+def stage1():
+    first = input()
+    second = [dic[x] for x in first]
+    print(''.join(second))
 
+def stage2():
+    dna = input().split(' ')
+    fr1 = dna[0].replace("CTGCAG", "C TGCAG")
+    fr2 = dna[1].replace("GACGTC", "GACGT C")
+    print(fr1)
+    print(fr2)
+
+stage2()
